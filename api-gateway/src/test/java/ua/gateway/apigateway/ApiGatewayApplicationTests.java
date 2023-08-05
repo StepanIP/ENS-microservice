50e6dff5cec8c111803eae340e5fe602427df44c
StepanIP <scrupnichuk@gmail.com>
2023-08-05 16:43:28 +0300

    created api-gateway

---
+++ api-gateway/src/test/java/ua/gateway/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.gateway.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
