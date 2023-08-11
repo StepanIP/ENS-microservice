f25956921b9546e2370031378fa00cdcf6bcdf2d
StepanIP <scrupnichuk@gmail.com>
2023-08-11 11:29:38 +0300

    created writer service for gaining data from user using xls or xlsx files

--- writer-service/src/main/java/org/example/WriterServiceApplication.java
+++ writer-service/src/main/java/org/example/WriterServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class WriterServiceApplication {
+class WriterServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WriterServiceApplication.class, args);
